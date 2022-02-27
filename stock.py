033c03bc111dec2bf0fc86ad8594847a23357457
ZAYAR SOE <zayarsoe@ayes-mbp.lan>
2022-02-27 10:52:43 -0800

    converting it into function

--- stock.py
+++ stock.py
@@ -29,68 +29,71 @@ N = "\033[0m" # Reset
 
 
 
+def stock_history_compare():
+
+    for stock in stock_list:
+
+        #Get latest price
+        print("Getting latest price for ", stock)
+        lprice = get_live_price(stock)
+        lprice = int(lprice)
+
+        print("Getting past price for ", stock)
+        df = get_data(stock, start_date = past_date, end_date = next_day)
+        hprice = df.iloc[0,1] #row and column from pandas dataframe
+        hprice = int(hprice)
+
+        #difference % calculation
+        diffprice = hprice - lprice
+
+        percent = float(diffprice) / float(hprice) * 100
+
+        percent = int(percent)
+        percent = -percent
+
+        #coloring the percent
+        if percent < 0:
+            color = R
+        else:
+            color = G
 
 
-for stock in stock_list:
+        #getting stats
+        print("Getting stats for ", stock)
+        try:
+            stat = get_stats_valuation(stock)
 
-    #Get latest price
-    print("Getting latest price for ", stock)
-    lprice = get_live_price(stock)
-    lprice = int(lprice)
-
-    print("Getting past price for ", stock)
-    df = get_data(stock, start_date = past_date, end_date = next_day)
-    hprice = df.iloc[0,1] #row and column from pandas dataframe
-    hprice = int(hprice)
-
-    #difference % calculation
-    diffprice = hprice - lprice
-
-    percent = float(diffprice) / float(hprice) * 100
-
-    percent = int(percent)
-    percent = -percent
-
-    #coloring the percent
-    if percent < 0:
-        color = R
-    else:
-        color = G
+            ps_val = stat.iloc[5,1] #getting price per sales
+            pb_val = stat.iloc[6,1] #getting price per book
 
 
-    #getting stats
-    print("Getting stats for ", stock)
-    try:
-        stat = get_stats_valuation(stock)
-
-        ps_val = stat.iloc[5,1] #getting price per sales
-        pb_val = stat.iloc[6,1] #getting price per book
-
-
-    except Exception:
-        # some stock symbols like ETF indexes have don't this value
-        ps_val = "N/A"
-        pb_val = "N/A"
-        pass
-
-
-
-    #get next earning date
-    print("Checking ER date for ", stock)
-    try:
-        er_date = get_next_earnings_date(stock)
-    except Exception:
-        # some stock symbols like ETF indexes have don't this value
-        er_date = "N/A"
-        pass
-
-
-    #printing the table , it only accept string , N to terminate the color
-
-    print("Adding row in table for ", stock)
+        except Exception:
+            # some stock symbols like ETF indexes have don't this value
+            ps_val = "N/A"
+            pb_val = "N/A"
+            pass
 
 
 
-    t.add_row([stock,"$"+str(lprice),"$"+str(hprice),color+str(percent)+"%"+N,ps_val,pb_val,er_date])
+        #get next earning date
+        print("Checking ER date for ", stock)
+        try:
+            er_date = get_next_earnings_date(stock)
+        except Exception:
+            # some stock symbols like ETF indexes have don't this value
+            er_date = "N/A"
+            pass
 
-print(t)
+
+        #printing the table , it only accept string , N to terminate the color
+
+        print("Adding row in table for ", stock)
+
+
+
+        t.add_row([stock,"$"+str(lprice),"$"+str(hprice),color+str(percent)+"%"+N,ps_val,pb_val,er_date])
+
+    print(t)
+
+if __name__ == '__main__':
+    stock_history_compare()
